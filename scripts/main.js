139eeb30037a0e848e7a6ba55b7a7812f58e8346
Ryuto Setsujin <32800772+RyutoSetsujin@users.noreply.github.com>
2023-08-27 20:29:30 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -187,7 +187,7 @@ cv.text("(controls are DFJK.)", `#FFFFFFA0`, 768, 600, "pixel", "40", "center");
 
 cv.text(tips[tipnum], ["#FF8080", "#80FFFF"], 768, 715, "pixel2", "35", "center")
 
-cv.text("0.1 Staging\nhttps://discord.gg/2D2XbD77HD", "#DDDDDD50", 0, 30, "monospace", "25", "left");
+cv.text("0.01 Staging\nhttps://discord.gg/2D2XbD77HD", "#DDDDDD50", 0, 30, "monospace", "25", "left");
 break;
 
 //song select
